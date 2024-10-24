5df6cc3248f8d16a92747e0cd7b284455209b309
André Morales <labmorales@gmail.com>
2024-10-24 17:23:23 -0300

    feat(CWC-4286): adding since option to add command

---
+++ .changeset/wicked-lemons-listen.md
@@ -0,0 +1,5 @@
+---
+"@cosm-changesets/cli": minor
+---
+
+Adding the since attribute to the add command
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,7 +10,7 @@ const { input, flags } = meow(
     $ changeset [command]
   Commands
     init
-    add [--empty] [--open]
+    add [--empty] [--open] [--since <branch>]
     version [--ignore] [--snapshot <?name>] [--snapshot-prerelease-template <template>]
     publish [--tag <name>] [--otp <code>] [--no-git-tag]
     status [--since <branch>] [--verbose] [--output JSON_FILE.json]
